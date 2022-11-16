f496465d1667a418d5480ce0953b9d07ca1849d8
ManoelFer <manoelfernandes15@gmail.com>
2022-11-16 10:05:21 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <Text>Minha tela de Swap de Cripto</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/components/InputCrypto/index.tsx
@@ -0,0 +1,13 @@
+import { TextInput } from "react-native"
+import { ViewStyled } from "./styles"
+
+
+export const InputCrypto = () => {
+    return (
+        <ViewStyled>
+            <TextInput placeholder="0" />
+
+            <TextInput placeholder="0" />
+        </ViewStyled>
+    )
+}
